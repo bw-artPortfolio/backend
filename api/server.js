ceb38d2271884f46f89b0c670e944fe7d8fe5971
Ben <allenben@protonmail.com>
2019-09-23 10:58:57 -0500

    Initial commit

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const server = express();
-server.use(express.json()); //
+server.use(express.json()); ////
 
 const cors = require('cors');
 server.use(cors());
