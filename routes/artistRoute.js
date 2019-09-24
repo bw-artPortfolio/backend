804dba17af22a82240721158eda294cda2ee0083
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-09-24 15:11:58 -0700

    artists route works

--- routes/artistRoute.js
+++ routes/artistRoute.js
@@ -5,8 +5,9 @@ const artists = require('../database/artistModel');
 const entries = require('../database/entryModel');
 
 router.get('/:id', async (req, res) => {
+    const artistId = req.params.id;
     try {
-        const { id, username } = await artists.findBy({ id });
+        const { id, username } = await artists.findBy({ id: artistId });
         const userEntries = await entries.byUser(id);
         const { id: u, username_id, description, title, imgURL, votes} = userEntries;
         const strippedEntries = userEntries.map(({ id, imgURL, title}) => {
@@ -24,8 +25,9 @@ router.get('/:id', async (req, res) => {
         });
     }
     catch (err) {
+        console.log(err);
         res.status(500).json(
-            { message: 'failed to load artist info from database'}
+            { message: 'failed to load artist info from database' }
         );
     }
 });
