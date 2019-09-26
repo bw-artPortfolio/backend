3a7274a18bb054bb10109a75d012c6c91d75a766
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-09-26 12:56:18 -0700

    WIP: testing

--- server.spec.js
+++ server.spec.js
@@ -7,10 +7,28 @@ const artistModel = require('./database/artistModel');
 const entryModel = require('./database/entryModel');
 
 describe('account route', () => {
-    test('hello world', async () =>{
+    test('hello world', async () => {
         const res = await request(server).get('/');
 
         expect(res.status).toEqual(200);
+    });
+
+    test('register', async () => {
+        const { artists: initArtists } = await request(server)
+            .get('/api/artists');
+        const res = await request(server)
+            .post('/api/account/register')
+            .send({
+                username: "testone",
+                password: "3141592"
+            });
+        const { artists } = await request(server)
+            .get('/api/artists');
+
+        expect(artists.length).toEqual(initArtists.length + 1);
+        expect(artists.some(artist => {
+            return artist.username === 'testone';
+        })).toBeTruthy();
     });
 
 });
