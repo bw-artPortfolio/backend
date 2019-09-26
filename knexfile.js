3c1fd156c306bc9252dd9bdfd2d6199e5dcaa590
Ben <allenben@protonmail.com>
2019-09-26 15:49:25 -0500

    Commented out CASCADE on UPDATE & DELETE - renamed data => database in knexfile

--- knexfile.js
+++ knexfile.js
@@ -26,10 +26,10 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './data/migrations',
+      directory: './database/migrations',
     },
     seeds: {
-      directory: './data/seeds',
+      directory: './database/seeds',
     },
   },
 };
